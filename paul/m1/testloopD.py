d1f238888983cb57e31e56da8170b35f2624c37e
Paul Rozdeba <paul.rozdeba@gmail.com>
2013-11-29 22:39:01 -0500

    debugged automation code

--- paul/m1/testloopD.py
+++ paul/m1/testloopD.py
@@ -1,5 +1,6 @@
 from mpl_toolkits.mplot3d import Axes3D
 
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
@@ -9,7 +10,7 @@ Nensm = 100
 
 # rescale variance of matrix elements
 J2 = 1.05*1.05
-Jvar = J2/N
+Jvar = J2/Nelem
 
 # equations of motion
 g = 1.0
@@ -17,23 +18,25 @@ def dxdt(x, t, g, J):
     return -x + np.einsum('ij,j', J, np.tanh(g*x))
 
 for nrun in range(Nensm):
+    print str(nrun)
     # generate instance of random matrix
-    J_orig = np.random.randn(N,N)
+    J_orig = np.random.randn(Nelem,Nelem)
     J = np.sqrt(Jvar) * J_orig
     
-    for i in range(N):
+    for i in range(Nelem):
         J[i,i] = 0.0
     
     # randomly draw initial state
-    x = np.array(np.random.randn(N))
+    x = np.array(np.random.randn(Nelem))
     # instead use x on the limit cycle found in one set of experiments
     #x = np.load('limitcycle_x0.npy')
     
     # let the transient run out
     tstart = time.time()  # timing purposes
+    print 'Running out transient...'
     t0 = 0.0
-    tf = 10000.0  # assumed transient time
-    dt = 0.1
+    tf = 20000.0  # assumed transient time
+    dt = 1.0
     times = np.arange(t0, tf+dt, dt)
     
     sol = odeint(dxdt, x, times, (g,J))
@@ -43,8 +46,9 @@ for nrun in range(Nensm):
     
     # now run for a little while to collect data
     tstart = time.time()  # timing purposes
+    print 'Running data collection period...'
     t0 = 0.0
-    tf = 1000.0  # assumed transient time
+    tf = 5000.0  # assumed transient time
     dt = 0.1
     times = np.arange(t0, tf+dt, dt)
     
@@ -65,17 +69,17 @@ for nrun in range(Nensm):
     ax.plot(times, sol.T[3])
     ax = fig.add_subplot(5,1,5)
     ax.plot(times, sol.T[4])
-    fig.savefig('figs_testloop/ts_J_' + str(J2) + '_run' + str(nrun) + '.pdf'
+    fig.savefig('figs_testloop/ts_J_' + str(J2) + '_run' + str(nrun) + '.pdf')
     
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     ax.plot(sol.T[0],sol.T[1])
-    fig.savefig('figs_testloop/ps_J_' + str(J2) + '_run' + str(nrun) + '.pdf'
+    fig.savefig('figs_testloop/ps_J_' + str(J2) + '_run' + str(nrun) + '.pdf')
     
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot(sol.T[0],sol.T[1],sol.T[2])
-    fig.savefig('figs_testloop/ps_J_' + str(J2) + '_run' + str(nrun) + '_3d.pdf'
+    fig.savefig('figs_testloop/ps_J_' + str(J2) + '_run' + str(nrun) + '_3d.pdf')
     
     
     
